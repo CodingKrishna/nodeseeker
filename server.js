e011891123720ad99efe15c8e31d1a9dbac68bea
terriblebassist <asingh175@gmail.com>
2021-05-19 21:01:22 +0530

    code refactor

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const fundCache = new cache.Cache()
 app.use('/', router)
 dotenv.config()
 
-const URL = 'https://www.amfiindia.com/spages/NAVAll.txt'
-const FUNDS_KEY = "funds"
+const AMFI_WEBSITE = 'https://www.amfiindia.com/spages/NAVAll.txt'
+const AMFI_FUND_REGEX = '(.*);(.*);(.*);(.*);(.*);(.*)'
 const TIMEOUT = parseInt(process.env.TIMEOUT ?? 1800000)
 const COUNTRY = process.env.COUNTRY ?? "en-IN"
 const TIMEZONE = process.env.TIMEZONE ?? "Asia/Kolkata"
 
 
-async function fetchRawData() {
+async function fetchWebpage() {
     let webpage_text = ''
     try {
-        let webpage = await fetch(URL)
+        let webpage = await fetch(AMFI_WEBSITE)
         webpage_text = await webpage.text()
     } catch (e) {
         console.error(e)
@@ -31,47 +31,46 @@ async function fetchRawData() {
 function extractFunds(webpage) {
     let lines = webpage.split(/\r\n|\r|\n/)
     lines = lines.filter(element => element !== ' ')
-    let funds = {}
+    let funds = []
     lines.forEach(element => {
-        let re = new RegExp('(.*);(.*);(.*);(.*);(.*);(.*)');
+        let re = new RegExp(AMFI_FUND_REGEX);
         if (re.test(element)) {
             let attrs = element.match(re)
-            funds[attrs[1]] = {
+            funds.push({
                 'schemeCode': attrs[1],
                 'schemeName': attrs[4],
                 'nav': attrs[5],
                 'date': attrs[6],
-                'updatedAt': new Date(Date.now()).toLocaleString(
-                    COUNTRY,
-                    {"timeZone": TIMEZONE}
-                )
-            }
+                'updatedAt': new Date(Date.now())
+                    .toLocaleString(COUNTRY, { "timeZone": TIMEZONE })
+            })
         }
     });
     return funds
 }
 
+async function reloadCacheAndFetch(scheme_code) {
+    console.log("Reloading cache...");
+    fundCache.clear()
+    let rawtext = await fetchWebpage()
+    let funds = extractFunds(rawtext)
+    funds.forEach(fund => {
+        fundCache.put(fund.schemeCode, JSON.stringify(fund), TIMEOUT)
+    })
+    return fundCache.get(scheme_code)
+}
+
 app.get('/getNAV/:scheme_code', async (req, res) => {
     const { scheme_code } = req.params
     const { force_update } = req.query
-    console.log(`Request for NAV, scheme code:${scheme_code}, force update:${force_update}`);
 
-    if (force_update === 'true') {
-        fundCache.clear()
+    console.log(`Fetching scheme code:${scheme_code}, force update:${force_update}`);
+    let entry = fundCache.get(scheme_code)
+    if (entry == null || force_update === 'true') {
+        entry = await reloadCacheAndFetch(scheme_code)
     }
-    const entry = fundCache.get(FUNDS_KEY)
-    if (entry) {
-        console.log("Successfully retrieved from cache");
-        res.status(200).send(JSON.parse(entry)[scheme_code])
-    } else {
-        console.log("Fetching from API");
-        let navdata = await fetchRawData()
-        let funds = extractFunds(navdata)
-        fundCache.put(FUNDS_KEY, JSON.stringify(funds), TIMEOUT)
-        res.status(200).send(funds[scheme_code])
-    }
+    console.log(`Sending response: ${entry}`);
+    res.status(200).send(JSON.parse(entry))
 })
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('Server is up!')
-})
+app.listen(process.env.PORT || 5000, () => { console.log('Nodeseeker GO!') })
